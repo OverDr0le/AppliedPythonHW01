d5d1bba2a233d95c4ed0b03ea640d54860c51ca4
Daniil Yakusik <dvyakusik@edu.hse.ru>
2025-12-18 20:58:04 +0300

    Все задания выполнены

--- app/modules/requests_worker.py
+++ app/modules/requests_worker.py
@@ -1,12 +1,11 @@
-from datetime import date
 import httpx
 
 async def get_async_response(cityname,key):
     '''
-    Эта функция возвращает json response и текущую дату
+    Эта функция возвращает json response
     '''
     url = f"http://api.openweathermap.org/data/2.5/weather?q={cityname}&appid={key}&units=metric"
     async with  httpx.AsyncClient() as client:
         response = await client.get(url)
     data = response.json()
-    return data,date.today()
+    return data
